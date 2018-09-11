4999355b7cec0a6471e34f9b240d8296e83f0e43
vinzentp <43040134+vinzentp@users.noreply.github.com>
2018-09-11 13:57:50 +0200

    commented out class-definition

--- js/resource_system.js
+++ js/resource_system.js
@@ -1,4 +1,47 @@
-class ResourcesSystem{
+
+
+	let oxygen = 21;
+	let carbondioxid = 0.04;
+	let energy = 150;
+	let time = 90;
+	let N = 0;
+	let nIdle=0;
+	let nWork=0;
+	let v = 1;
+	let w = 100;
+	let productivity = 0;
+	let energyConsumption = 0;
+
+let timeUnit = setInterval(myTime, 1000);
+
+	function myTime() {
+		let oxygenFactor = 4.76/100;
+		//let carbondioxidFactor = 
+		//productivity = v*(oxygen*oxygenFactor)*(carbondioxid*)
+		oxygen -=1;
+		carbondioxid +=1;
+		energy -= 1;
+		document.querySelector('.energy-value').innerHTML=energy+'/150 HU';
+
+
+	}
+
+	myTime();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*class ResourcesSystem{
 
 	let oxygen = 21;
 	let carbondioxid = 0.04;
@@ -34,7 +77,4 @@ class ResourcesSystem{
 	}
 
 
-}
-
-let System = new ResourcesSystem(200);
-ResourcesSystem.myTime();
+}*/
