abf78dd74325efdb79392f1d5e04828a1ed7ba08
MauMau345 <j.turnwald@web.de>
2018-09-11 18:28:12 +0200

    Change spotlight position

--- js/lights.js
+++ js/lights.js
@@ -1,17 +1,24 @@
-function getSpotlight() {
+function getSpotLight() {
 	let spotLight = new THREE.SpotLight( 0xffffff );
-	spotLight.position.set( 0, 0, 40 );
+	spotLight.position.set( 0, 200, -400 );
 	spotLight.lookAt(0,0,0);
 
 	spotLight.castShadow = true;
 	spotLight.penumbra = 0.05;
 	spotLight.decay = 2;
 	spotLight.distance = 200;
+	spotLight.angle = 0.98;
 	spotLight.shadow.mapSize.width = 1024;
 	spotLight.shadow.mapSize.height = 1024;
 	spotLight.shadow.camera.near = 10;
-	spotLight.shadow.camera.far = 200;
+	spotLight.shadow.camera.far = 1000;
 	
 	return spotLight;
 }
 
+function getHemiLight(){
+	let hemiLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1);
+	return hemiLight;
+	
+	
+}
