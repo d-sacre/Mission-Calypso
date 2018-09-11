57e2f1b8ce163330ef2669f0738b4ad35191ca12
oskarocean <43137183+oskarocean@users.noreply.github.com>
2018-09-11 15:16:56 +0200

    added carbondioxidFactorCalculation() and its value test-log

--- js/resource_system.js
+++ js/resource_system.js
@@ -62,18 +62,36 @@ class ResourcesSystem{
 		
 	myTime() {
 		let oxygenFactor = 4.76/100;
-		//let carbondioxidFactor = 
+		let carbondioxidFactor = carbondioxidFactorCalculation();
 		//productivity = this v*(this oxygen*oxygenFactor)*(this carbondioxid*)
 		this.oxygen -=1;
 		this.carbondioxid +=1;
 		this.energy -= 1;
 		this.time += 1;
+		
+		console.log(carbondioxidFactor);
 
 		return {
 			time: this.time,
 			energy: this.energy,
 		};
 	}
+	
+	carbondioxidFactorCalculation(){
+		carbondioxid=this.carbondioxid;
+		  if(0.04<=carbondioxid<=0.08){
+		    return (1-2.5*(carbondioxid-0.04));
+		  }
+		  if(carbondioxid<=1.5){
+		      return (0.9-(20/141)*(carbondioxid-0.09));
+		  }
+		  if(carbondioxid<=5){
+		    return (0.7-(40/349)*(carbondioxid-1.51));
+		  } else {
+		      return (0.3-(3/29)*(carbondioxid-1.51));
+		  }
+	}
+	
 }
 let system = new ResourcesSystem(200);
 let timeUnit = setInterval(setTime, 1000);
