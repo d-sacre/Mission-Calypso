12e1929ea419c29fe4ef914a4a7952ff68894f07
MauMau345 <j.turnwald@web.de>
2018-09-12 19:53:50 +0200

    Implement mouse interaction

--- js/scene.js
+++ js/scene.js
@@ -1,8 +1,16 @@
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2(), intersected;
+var figure, spotLight;
 var container;
 var camera, controls, scene, renderer;
 var sky, sunSphere;
+
+
+
 init();
 animate();
+
+
 function initSky() {
 	// Add Sky
 	sky = new Sky();
@@ -23,11 +31,11 @@ function initSky() {
 		mieCoefficient: 0.051,
 		mieDirectionalG: 0.981,
 		luminance: 1.1,
-		inclination: 0.4207, // elevation / inclination
-		azimuth: 0.2256, // Facing front,
+		inclination: 0.4, // elevation / inclination
+		azimuth: 0.3, // Facing front,
 		sun: ! true
 	};
-	var distance = 400000;
+	var distance = 40000000000;
 	function guiChanged() {
 		var uniforms = sky.material.uniforms;
 		uniforms.turbidity.value = effectController.turbidity;
@@ -46,26 +54,34 @@ function initSky() {
 	}
 	guiChanged();
 }
+
+
+
 function init() {
+	
+	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 10, window.innerWidth / window.innerHeight, 30, 2000000 );
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	
 	//PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
-	camera.position.set( 0, 10, 100 );
+	camera.position.set( 0, 3*BOXSIZE.x, 100*BOXSIZE.x );
 	camera.lookAt( 0, 0, 0 );
 	//camera.setLens(20);
-	scene = new THREE.Scene();
-	var helper = new THREE.GridHelper( 10000, 2, 0xffffff, 0xffffff );
-	scene.add( helper );
-	renderer = new THREE.WebGLRenderer();
+
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 	controls = new THREE.OrbitControls( camera, renderer.domElement );
 	controls.addEventListener( 'change', render );
-	//controls.maxPolarAngle = Math.PI / 2;
+	controls.maxPolarAngle = Math.PI / 2;
 	controls.enableZoom = false;
 	controls.enablePan = false;
 	initSky();
-		let spotLight = getSpotLight();
+	
+	var helper = new THREE.GridHelper( 10000, 2, 0xffffff, 0xffffff );
+	scene.add( helper );
+	
+	spotLight = getSpotLight();
 	scene.add( spotLight );
 	
 	let hemiLight = getHemiLight();
@@ -73,22 +89,99 @@ function init() {
 	
 	let lightHelper = new THREE.SpotLightHelper( spotLight );
 	scene.add( lightHelper );
-		let model = buildModel();
-	scene.add(model);	
+	
+	let model = buildModel();
+	scene.add(model);
+	
+	figure =buildFigure();
+	scene.add(figure);
+	
+
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.addEventListener( 'click', onMouseClick, false);
+	
 	window.addEventListener( 'resize', onWindowResize, false );
 }
+
+function onMouseClick(event) {
+	event.preventDefault();
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+	
+	let targetBlocks = scene.children[6].children[1]
+	var intersections = raycaster.intersectObjects( targetBlocks.children );
+	
+	if(intersections.length > 0) {
+		console.log(intersections[0].object.name);
+		let pos = intersections[0].object.position.clone();
+		pos.z += 200;
+		figure.position.copy(pos);
+		pos.x += 100;
+		spotLight.position.copy(pos);
+		pos.x += 100;
+		pos.z -= 100;
+		spotLight.lookAt(pos);
+	}
+	
+}
+
+function onDocumentMouseMove( event ) {
+	event.preventDefault();
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	render();
 }
+
+
+
 function render() {
+	// update the picking ray with the camera and mouse position
+	
+	raycaster.setFromCamera( mouse, camera );
+
+	// calculate objects intersecting the picking ray
+	let targetBlocks = scene.children[6].children[1]
+	var intersections = raycaster.intersectObjects( targetBlocks.children );
+	var texSurface = new THREE.TextureLoader().load( "https://raw.githubusercontent.com/vinzentp/Mission-Calypso/graphics/pictures/textures/sand_highres_top_512x512_compress.jpg" );
+	//console.log(intersects[0].object);
+	//console.log(intersects[0].object);
+	
+
+		/*
+	if(intersects[0].object.name !== "") {
+	//targetBlocks.remove(intersects[0].object);
+	
+	intersects[0].object.material.emissive.setHex( 0xff0000 );
+
+	}
+*/
+			if ( intersections.length > 0 ) {
+				if ( intersected != intersections[ 0 ].object ) {
+					if ( intersected ) intersected.material.color.setHex( 0xffffff );
+					intersected = intersections[ 0 ].object;
+					intersected.material.color.setHex( 0xff0000 );
+				}
+				//document.body.style.cursor = 'pointer';
+			}
+			else if ( intersected ) {
+				intersected.material.color.setHex( 0xffffff );
+				intersected = null;
+				//document.body.style.cursor = 'auto';
+			}
+	
+
 	renderer.render( scene, camera );
 }
 
 function animate() {
 	requestAnimationFrame( animate );
+	render();
 	controls.update();
 	renderer.render( scene, camera );
 }
