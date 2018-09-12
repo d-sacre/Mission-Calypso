8facbab233e5828b16cdf371cc34f13c1080333a
oskarocean <43137183+oskarocean@users.noreply.github.com>
2018-09-12 18:01:58 +0200

    added gui-button-logic for menu interaction

---
+++ js/button_interaction.js
@@ -0,0 +1,7 @@
+document.querySelector('#to-rocket-menu-popup').addEventListener("click", function() {
+  document.querySelector('.transform').classList.toggle('transform-active');
+});
+
+document.querySelector('#return-to-game-popup-button').addEventListener("click", function() {
+  document.querySelector('.transform').classList.toggle('transform-active');
+});
