95f2fe63c3514969c8bdd1c96e7af92b9aca7ec5
oskarocean <43137183+oskarocean@users.noreply.github.com>
2018-09-12 23:36:48 +0200

    added simple drag'n'drop-functions

--- js/button_interaction.js
+++ js/button_interaction.js
@@ -5,3 +5,17 @@ document.querySelector('#to-rocket-menu-popup').addEventListener("click", functi
 document.querySelector('#return-to-game-popup-button').addEventListener("click", function() {
   document.querySelector('.transform').classList.toggle('transform-active');
 });
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
