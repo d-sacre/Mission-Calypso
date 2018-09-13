a45f2f0b6cf82f6e1dd1ca9cccd4c1136a38d032
MauMau345 <43137096+MauMau345@users.noreply.github.com>
2018-09-13 10:35:41 +0200

    Added drill function

--- js/actions.js
+++ js/actions.js
@@ -10,8 +10,32 @@ function deleteCubeUnderground(x, y){
 	group.remove(cube);
 }
 
+function deleteCubeEdge(x){
+	let cube = scene.getObjectByName( "(" + x + ")");
+	groupSurface.remove(cube);
+}
+
+
 function walk(x, y) {
-	
-	
-	
+	let fig = scene.getObjectByName( "Fig1");
+	fig.translateX(x);
+}
+
+function drill(fuel) {
+	for(let x=-1; x<2; x++){
+			deleteCubeEdge(x);
+	}
+
+	for(let x=-1; x<2; x++){
+		for (let y=0; y<3; y++){
+			deleteCubeUnderground(x, y);
+		}
+	}
+}
+
+function sporn(){
+	drill();
+	let fig = scene.getObjectByName( "Fig1");
+	var posFig = new THREE.Vector3(0*BOXSIZE.x, -2*BOXSIZE.y, 0);
+	meshFig.position.copy(posFig);	
 }
