11903dada4747b70c0db6ba2a14be7d4c0949bf4
ChriGi03 <christina.gihl@stud.uni-regensburg.de>
2018-10-02 18:18:34 +0200

    Change position and size

--- js/threejs/figure.js
+++ js/threejs/figure.js
@@ -2,11 +2,11 @@
 function buildFigure(){
 	//var texUnderground = new THREE.TextureLoader().load( "https://raw.githubusercontent.com/vinzentp/Mission-Calypso/graphics/pictures/textures/granite_highres_withoutedge_256x256_compress.jpg" );
 	var matFig = new THREE.MeshPhongMaterial( {color:0xff0000, wireframe: false});
-	var geoFig = new THREE.BoxBufferGeometry( BOXSIZE.x/2 , BOXSIZE.y/2,  BOXSIZE.z/2);
-	meshFig = new THREE.Mesh( geoFig, matFig );
+	let geoFig = new THREE.BoxBufferGeometry( BOXSIZE.x/2 , BOXSIZE.y/2,  BOXSIZE.z/2);
+	let meshFig = new THREE.Mesh( geoFig, matFig );
 
 	
-	var posFig = new THREE.Vector3(0*BOXSIZE.x, 1.5*BOXSIZE.y, BOXSIZE.y*0);
+	let posFig = new THREE.Vector3(0*BOXSIZE.x, 1.5*BOXSIZE.y, BOXSIZE.y*0);
 	meshFig.position.copy(posFig);
 	let name = "Fig1";
 	meshFig.name= name;
@@ -18,13 +18,13 @@ function buildFigure(){
 function buildDrill(){
 	let texDrill = new THREE.TextureLoader().load( "https://raw.githubusercontent.com/vinzentp/Mission-Calypso/abgabe/pictures/textures/pink-and-green-lined-background.jpg" );
 	let matDrill = new THREE.MeshPhongMaterial( {  wireframe: true});
-	var geoDrill = new THREE.CylinderBufferGeometry( 0.5*BOXSIZE.x, 0*BOXSIZE.x, 1*BOXSIZE.x, 5 );
-	let meshDrill;
-	meshDrill = new THREE.Mesh( geoDrill, 	matDrill );
+	let geoDrill = new THREE.CylinderBufferGeometry( 1.5*BOXSIZE.x, 0*BOXSIZE.x, 1*BOXSIZE.x, 5 );
+	let meshDrill = new THREE.Mesh( geoDrill, matDrill );
 	//meshDrill.rotateY( Math.sign(x)* Math.PI / 5.4 );
 	meshDrill.receiveShadow = true;
 	meshDrill.castShadow = true;
-	let posDrill = new THREE.Vector3(0, 0, BOXSIZE.z*(4.0));
+	let posDrill = new THREE.Vector3(0, 0, BOXSIZE.z*(1));
+	meshDrill.position.copy(posDrill);
 	let name = "Drill";
 	meshDrill.name= name;
 	return meshDrill;
