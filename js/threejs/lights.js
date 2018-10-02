44fba7723e927864ae946dbf74aa6ef7841333f4
ChriGi03 <christina.gihl@stud.uni-regensburg.de>
2018-10-03 01:31:10 +0200

    Implement directionalLight

--- js/threejs/lights.js
+++ js/threejs/lights.js
@@ -4,3 +4,9 @@ function getHemiLight(){
 	hemiLight.position.set(0,100,000);
 	return hemiLight;
 }
+
+function getDirectLight(){
+	var directLight = new THREE.DirectionalLight( 0xffffff, 1 );
+	directLight.position.set( 0, 100, 100 );
+	return directLight;
+}
