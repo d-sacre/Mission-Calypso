f9b0f6d8dac57653ab3a2498d03223f44a6e85b8
oskarocean <43137183+oskarocean@users.noreply.github.com>
2018-10-02 11:00:37 +0200

    fixed decarbonizer slider refresh routine
    decarbonizer slider innerHTML will now be updated by click on the button, so it is always displaying the right numbers immediately.

--- js/userinteraction/game_button_interaction.js
+++ js/userinteraction/game_button_interaction.js
@@ -12,18 +12,18 @@ document.querySelector('#game-over-back-to-mainmenu-popup-button').addEventListe
 /* function for buttons*/
 function gameGUIPopupMenuAnimation(){
   // every time when gameGUIPopup should appear, unbind three.js mouse-event listeners
+  // event listeners defined in js/threejs/scene.js
   document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
   document.removeEventListener( 'click', onMouseClick, false);
-  // start css-animation for slide-up of menu
-  document.querySelector('.transform').classList.toggle('transform-active');
-  if (document.querySelector('#game-gui-popup').classList[2]!='transform-active') {//if menu not sliding up or visible
-    document.addEventListener('mousemove', onDocumentMouseMove, false); // recreate three.js mouse-event listeners
+  document.querySelector('.transform').classList.toggle('transform-active'); // start css-animation for slide-up of menu
+  if (document.querySelector('#game-gui-popup').classList[2]!='transform-active') { //if menu not sliding up or visible
+    document.addEventListener('mousemove', onDocumentMouseMove, false);  // recreate three.js mouse-event listeners
   	document.addEventListener('click', onMouseClick, false);
   }
 }
-/* attach event listeners to buttons */
-document.querySelector('#to-rocket-menu-clickbox').addEventListener("click",gameGUIPopupMenuAnimation);
 
+/* attach event listeners to slide up/down buttons/clickboxes for menu*/
+document.querySelector('#to-rocket-menu-clickbox').addEventListener("click",gameGUIPopupMenuAnimation);
 document.querySelector('#return-to-game-popup-button').addEventListener("click", gameGUIPopupMenuAnimation);
 
 /* button for carbonizer useage */
@@ -32,6 +32,7 @@ document.querySelector('#use-carbonizer-button').addEventListener("click", funct
   document.querySelector('#decarbonizer-storage-value').value=document.querySelector('#decarbonizer-storage-value').value-document.querySelector('#use-decarbonizer-slider').value // substract number of used decarbonizers from storage
   document.querySelector('#decarbonizer-storage-value').innerHTML=document.querySelector('#decarbonizer-storage-value').value+'/15 kg'; // display changes
   document.querySelector('#use-decarbonizer-slider').value=0; // reset desired number of decarbonizers to 0
-  document.querySelector('#use-decarbonizer-slider').max=document.querySelector('#decarbonizer-storage-value').value;
+  document.querySelector('#use-decarbonizer-slider').max=document.querySelector('#decarbonizer-storage-value').value; // set maximum slider value to available decarbonizers in storage
+  document.querySelector('#use-decarbonizer-slidervalue').innerHTML=document.querySelector('#use-decarbonizer-slider').value+'/'+document.querySelector('#decarbonizer-storage-value').value; // write actual value of slider and max into gui
   document.querySelector('#carbondioxide-afteruseitem-value').value=0.04; // reset value for possible reduction
 });
