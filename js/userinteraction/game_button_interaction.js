7f064e0bb2c2d90aa442dd8ce2a80b48dafd8851
oskarocean <43137183+oskarocean@users.noreply.github.com>
2018-10-02 10:48:26 +0200

    added preventclickthrough-functionality when game-gui-popup is transform-active
    preventclickthrough-functionality: three.js mouse actions are stopped (removes eventhandler), if game-gui-popup is active. Handlers will be re-established when game-gui-popup hidden again.

--- js/userinteraction/game_button_interaction.js
+++ js/userinteraction/game_button_interaction.js
@@ -9,13 +9,22 @@ document.querySelector('#game-over-back-to-mainmenu-popup-button').addEventListe
 });
 
 /* buttons/clickboxes for slide up/down of menu */
-document.querySelector('#to-rocket-menu-clickbox').addEventListener("click", function() {
+/* function for buttons*/
+function gameGUIPopupMenuAnimation(){
+  // every time when gameGUIPopup should appear, unbind three.js mouse-event listeners
+  document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.removeEventListener( 'click', onMouseClick, false);
+  // start css-animation for slide-up of menu
   document.querySelector('.transform').classList.toggle('transform-active');
-});
+  if (document.querySelector('#game-gui-popup').classList[2]!='transform-active') {//if menu not sliding up or visible
+    document.addEventListener('mousemove', onDocumentMouseMove, false); // recreate three.js mouse-event listeners
+  	document.addEventListener('click', onMouseClick, false);
+  }
+}
+/* attach event listeners to buttons */
+document.querySelector('#to-rocket-menu-clickbox').addEventListener("click",gameGUIPopupMenuAnimation);
 
-document.querySelector('#return-to-game-popup-button').addEventListener("click", function() {
-  document.querySelector('.transform').classList.toggle('transform-active');
-});
+document.querySelector('#return-to-game-popup-button').addEventListener("click", gameGUIPopupMenuAnimation);
 
 /* button for carbonizer useage */
 document.querySelector('#use-carbonizer-button').addEventListener("click", function() {
@@ -23,5 +32,6 @@ document.querySelector('#use-carbonizer-button').addEventListener("click", funct
   document.querySelector('#decarbonizer-storage-value').value=document.querySelector('#decarbonizer-storage-value').value-document.querySelector('#use-decarbonizer-slider').value // substract number of used decarbonizers from storage
   document.querySelector('#decarbonizer-storage-value').innerHTML=document.querySelector('#decarbonizer-storage-value').value+'/15 kg'; // display changes
   document.querySelector('#use-decarbonizer-slider').value=0; // reset desired number of decarbonizers to 0
+  document.querySelector('#use-decarbonizer-slider').max=document.querySelector('#decarbonizer-storage-value').value;
   document.querySelector('#carbondioxide-afteruseitem-value').value=0.04; // reset value for possible reduction
 });
