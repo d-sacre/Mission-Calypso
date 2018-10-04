8b2865b109ff24aa54f759a70cfa9aa1833022ad
oskarocean <43137183+oskarocean@users.noreply.github.com>
2018-10-04 20:50:47 +0200

    implemented display-auto-refresh for 2nd row of storage

--- js/resourcesystem/resource_system_storage-and-refinery.js
+++ js/resourcesystem/resource_system_storage-and-refinery.js
@@ -1,6 +1,9 @@
 function updateStorageDisplay(){
-    document.querySelector('.energy-storage-value').innerHTML=document.querySelector('.energy-value').value+'/'+this.EnergyStartValue+' HU';
+    document.querySelector('.energy-storage-value').innerHTML=document.querySelector('.energy-value').value+' HU';
     document.querySelector('#raw-caloricum-storage-value').innerHTML=document.querySelector('#raw-caloricum-storage-value').value+'/15 t';
     document.querySelector('#copper-ore-value').innerHTML=document.querySelector('#copper-ore-value').value+'/15 t';
     document.querySelector('#pottasium-ore-value').innerHTML=document.querySelector('#pottasium-ore-value').value+'/30 kg';
+    document.querySelector('#copper-ingot-value').innerHTML=document.querySelector('#copper-ingot-value').value+' t';
+    document.querySelector('#decarbonizer-storage-value').innerHTML=document.querySelector('#decarbonizer-storage-value').value+' kg';
+
 }
