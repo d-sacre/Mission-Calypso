fff3b6b3bd9e7d8726da1772e81590017bb03fae
ChriGi03 <christina.gihl@stud.uni-regensburg.de>
2018-10-08 17:53:12 +0200

    Clearing

--- js/threejs/actions.js
+++ js/threejs/actions.js
@@ -11,6 +11,9 @@ function deleteCubeUnderground(x, y){
 	group.remove(cube);
 }
 
+
+
+
 function deleteCubeEdge(x){
 	let cube = scene.getObjectByName( "(" + x + ")");
 	groupSurface.remove(cube);
@@ -20,27 +23,12 @@ function deleteCubeEdge(x){
 
 
 function drill(stage) {
-	/*for(let x=-1; x<2; x++){
-			deleteCubeEdge(x);
-	}
-
-	for(let x=-1; x<2; x++){
-		for (let y=0; y<6; y++){
-			deleteCubeUnderground(x, y);
-		}
-	}
-	*/
-
-
 	let cubeEdge = scene.getObjectByName( "(" + 0 + ")");
 	if (cubeEdge != null){
 		deleteCubeEdge(0);
-	}else{
-		deleteCubeUnderground(0, stage);
 	}
-
-
 	
+	deleteCubeUnderground(0, stage);
 }
 
 
