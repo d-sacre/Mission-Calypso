5795741eb78f1b6bd83bf3d5e0efad53091e5543
oskarocean <43137183+oskarocean@users.noreply.github.com>
2018-10-08 11:26:55 +0200

    fixed wrong assignment of ore-id's

--- js/threejs/threejs_gui_interface.js
+++ js/threejs/threejs_gui_interface.js
@@ -45,15 +45,15 @@ function writeCurrentMainDrillPosition(position){
 
 // transfer mined resources to gui storage in html
 function storeMinedResources(resource){//resource=elementID (still to be defined); each resource block only contains 1 unit of material
-    if (resource==1){
+    if (resource==3){
         document.querySelector('#raw-caloricum-storage-value').value=parseFloat(document.querySelector('#raw-caloricum-storage-value').value)+1;
     }
 
-    if (resource==2){
+    if (resource==1){
         document.querySelector('#copper-ore-value').value=document.querySelector('#copper-ore-value').value+1;
     }
 
-    if (resource==3){
+    if (resource==2){
         document.querySelector('#pottasium-ore-value').value=document.querySelector('#pottasium-ore-value').value+1;
     }
 }
