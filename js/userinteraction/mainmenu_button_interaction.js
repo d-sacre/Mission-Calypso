bdc050966016e60c6350274ca24c623ded40faff
oskarocean <43137183+oskarocean@users.noreply.github.com>
2018-10-10 08:33:45 +0200

    added main menu button eventListeners

--- js/userinteraction/mainmenu_button_interaction.js
+++ js/userinteraction/mainmenu_button_interaction.js
@@ -4,3 +4,13 @@
 document.querySelector('#playgame-mainmenu-button').addEventListener("click", function(){
   window.location='./additionalhtml/game_intro.html';
 });
+
+/* Credits = redircet to credits.html */
+document.querySelector('#credits-mainmenu-button').addEventListener("click", function(){
+  window.location='./additionalhtml/credits.html';
+});
+
+/* Credits = redircet to help.html */
+document.querySelector('#help-mainmenu-button').addEventListener("click", function(){
+  window.location='./additionalhtml/help.html';
+});
